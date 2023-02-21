2b4fded701b755e1d5250027a5981ca02e04bc7b
David Romero <dromero@Davids-MacBook-Pro.local>
2023-02-20 19:10:11 -0600

    daily.sh added

---
+++ daily.sh
@@ -0,0 +1,17 @@
+# daily meeting
+open -a Safari $MEETING_URL
+# wait to continue
+
+# daily journal
+
+# slack
+#slack &
+
+# calendar
+#open -a Safari $CALENDAR_URL
+
+# tasks
+
+# notes
+
+# IDE selector
