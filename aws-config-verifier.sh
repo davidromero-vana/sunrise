b022d971b9a9159e663125631332a04fcf188f13
davidromero-vana <david.romero@vana.gt>
2023-03-24 01:25:27 -0600

    Added env to aws-config-verifier

--- aws-config-verifier.sh
+++ aws-config-verifier.sh
@@ -1,1 +1,13 @@
-aws sts get-caller-identity | jq --arg AWS_USER_NAME "$AWS_USER_NAME" '.UserId | contains($AWS_USER_NAME)'
+IDENTITY=$(aws sts get-caller-identity)
+if [ $(echo $IDENTITY | jq '.UserId' | grep -c $AWS_USER_NAME) -gt 0 ]; then
+  echo "Valid Credentials"
+  ACCOUNT_NUM=$(echo $IDENTITY | jq '.Account | tonumber')
+  AWS_ENV=$(case $ACCOUNT_NUM in
+  ($ACCOUNT_NUM_DEV) echo "DEV" ;;
+  ($ACCOUNT_NUM_QA) echo "QA" ;;
+  ($ACCOUNT_NUM_PROD) echo "PROD" ;;
+  esac)
+  echo $AWS_ENV" environment"
+else
+  echo "Invalid Credentials"
+fi
